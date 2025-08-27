74d5bb51fc1ea6ccf389eddbdeaa55b2686bfc86
samuel <samuelraj349@gmail.com>
2025-08-27 13:13:01 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 function add(a, b) {
   return a + b;
 }
-module.exports = add;
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  if (b == 0) {
+    return "b shouldn't be 0";
+  } else {
+    return a / b;
+  }
+}
+
+module.exports = { add, sub, mul, div };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,17 @@
-const add = require("./index");
+const { add, sub, mul, div } = require("./index");
 
 test("adds 2 + 3 to equal 5", () => {
   expect(add(2, 3)).toBe(5);
+});
+
+test("sub 3 - 1 to equal 2", () => {
+  expect(sub(3, 1)).toBe(2);
+});
+
+test("mul 2 * 3 to equal 6", () => {
+  expect(mul(2, 3)).toBe(6);
+});
+
+test("div 4 / 2 to equal to 2", () => {
+  expect(div(4, 2)).toBe(2);
 });
